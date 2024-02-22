c47b8eee9df67ecae41214d1b3b7abbb576103ed
jinho <jinho0547@naver.com>
2024-02-22 15:34:44 +0900

    kotlin practice

--- src/main/kotlin/condition/Condition.kt
+++ src/main/kotlin/condition/Condition.kt
@@ -3,7 +3,7 @@ package org.example.condition
 class Condition {
 }
 fun main(){
-    var a = 4
+    var a = 3
     var b = 6
 
     //if문은 자바와 동일
@@ -16,7 +16,10 @@ fun main(){
     //코틀린은 switch문 대신 when 문
     //가장 처음에 부합하는 구절만 실행
     when(a){
-        3 -> println("a = ${a}")
+        3 -> {
+            println("a = ${a}")
+            println("asdasd")
+        }
         in 4..6 -> println("4 <= a <= 6 ")
         4 -> println("위 조건에 먼저 부합하므로 해당 구문은 실행되지 않는다")
         is Int -> "정수형 데이터에 호환 가능합니다."
